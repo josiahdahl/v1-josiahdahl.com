fac238bb214fd5fd1af6f0158e7f7c08f197dd65
Josiah Dahl <josiah.dahl@gmail.com>
2017-02-20 11:19:27 -0800

    modified header & nav & portfolio look

--- _js/app.js
+++ _js/app.js
@@ -2,72 +2,76 @@
  * Created by jdahl on 5/10/16.
  */
 $(document).ready(function () {
-  $(".button-collapse").sideNav();
+    $(".button-collapse").sideNav({
+        menuWidth: 200,
+        edge: 'right',
+        draggable: true
+    });
 });
 
 (function () {
 
-  // Handle header movement
-  var header = $('.site-header');
-  var headerHeight = header.height();
-  window.addEventListener('resize', function (e) {
-    headerHeight = header.height();
-  });
-  // Fade the header image into the nav bar as we scroll past it
-  window.addEventListener('scroll', function (e) {
-    // Get header div hieght from top
-    var offset = $(window).scrollTop();
-    if (offset >= headerHeight / 2) {
-      $('.site-nav').addClass('navbar-fixed');
-      $('.site-nav').removeClass('site-nav_top');
-    }
-    else {
-      $('.site-nav').removeClass('navbar-fixed');
-      $('.site-nav').addClass('site-nav_top');
-    }
-  });
-  // REmove form error class on click
-  $('input, textarea').on('focus', function() {
-    $(this).parent().removeClass('form-error');
-  });
-
-  // handle form validation
-  $('#contact').on('submit', function (e) {
-    console.log('checksubmitted');
-    e.preventDefault();
-    var error = false;
-
-    // Required fields list
-    var reqFields = ['#name', '#email', '#message'];
-
-    reqFields.forEach(function(value, index) {
-      var el = $(value);
-      if (el.val() == '') {
-        el.parent().addClass('form-error');
-        error = true;
-      }
-      else if (value === '#email') {
-        // From http://emailregex.com/
-        var emailRegex = /.+@.+/i;
-        var validEmail = emailRegex.test(el.val());
-        if (!validEmail) {
-          el.parent().addClass('form-error');
-
-          error = true;
-        }
-      }
+    // Handle header movement
+    var header = $('.site-header');
+    var headerHeight = header.height();
+    window.addEventListener('resize', function (e) {
+        headerHeight = header.height();
     });
-    if (!error) {
-      $.ajax({
-        url: "https://formspree.io/josiahdahl+contact@gmail.com",
-        method: "POST",
-        data: $(this).serializeArray(),
-        dataType: "json"
-      });
-      document.getElementById('contact').reset();
-      $('.form-success').fadeIn();
-    }
+    // Fade the header image into the nav bar as we scroll past it
+    window.addEventListener('scroll', function (e) {
+        // Get header div hieght from top
+        var offset = $(window).scrollTop();
+        if (offset >= headerHeight / 2) {
+            $('.site-nav').addClass('navbar-fixed');
+            $('.site-nav').removeClass('site-nav_top');
+        }
+        else {
+            $('.site-nav').removeClass('navbar-fixed');
+            $('.site-nav').addClass('site-nav_top');
+        }
+    });
+    // REmove form error class on click
+    $('input, textarea').on('focus', function () {
+        $(this).parent().removeClass('form-error');
+    });
 
-  });
+    // handle form validation
+    $('#contact').on('submit', function (e) {
+        console.log('checksubmitted');
+        e.preventDefault();
+        var error = false;
+
+        // Required fields list
+        var reqFields = ['#name', '#email', '#message'];
+
+        reqFields.forEach(function (value, index) {
+            var el = $(value);
+            if (el.val() == '') {
+                el.parent().addClass('form-error');
+                error = true;
+            }
+            else if (value === '#email') {
+                // From http://emailregex.com/
+                var emailRegex = /.+@.+/i;
+                var validEmail = emailRegex.test(el.val());
+                if (!validEmail) {
+                    el.parent().addClass('form-error');
+
+                    error = true;
+                }
+            }
+        });
+        if (!error) {
+            $.ajax({
+                url: "https://formspree.io/josiahdahl+contact@gmail.com",
+                method: "POST",
+                data: $(this).serializeArray(),
+                dataType: "json"
+            });
+            document.getElementById('contact').reset();
+            $('.form-success').fadeIn();
+        }
+
+    });
 
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
  */
 
 // Gulpfile based on https://github.com/shakyShane/jekyll-gulp-sass-browser-sync
-  
+
 var gulp = require('gulp');
 var prefix = require('gulp-autoprefixer');
 var imagemin = require('gulp-imagemin');
@@ -18,15 +18,15 @@ var nano = require('gulp-cssnano');
 var sourcemaps = require('gulp-sourcemaps');
 
 
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages = {
-  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
 var jsIncludes = [
-  'bower_components/jquery/dist/jquery.js',
-  'bower_components/Materialize/dist/js/materialize.js',
-  '_js/app.js'
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/Materialize/dist/js/materialize.js',
+    '_js/app.js'
 ];
 
 
@@ -34,79 +34,85 @@ var jsIncludes = [
  * Build the Jekyll Site
  */
 gulp.task('jekyll-build', function (done) {
-  browserSync.notify(messages.jekyllBuild);
-  return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-    .on('close', done);
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn(jekyll, ['build'], {stdio: 'inherit'})
+        .on('close', done);
 });
 
 /**
  * Rebuild Jekyll & do page reload
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-  // browserSync.reload();
+gulp.task('jekyll-rebuild', ['jekyll-build'], function (done) {
+    browserSync.reload();
+    done();
 });
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['sass', 'scripts', 'jekyll-build'], function() {
-  browserSync({
-    server: {
-      baseDir: '_site'
-    },
-    open: false
-  });
+gulp.task('browser-sync', ['sass', 'scripts', 'jekyll-build'], function () {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        },
+        open: false
+    });
 });
 
 /**
  * Concat JS Files
  */
 
-gulp.task('scripts', function() {
-  return gulp.src(jsIncludes)
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest('./js/'))
-    .pipe(gulp.dest('./_site/js/'));
+gulp.task('scripts', function () {
+    return gulp.src(jsIncludes)
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./js/'))
+        .pipe(gulp.dest('./_site/js/'));
 });
 
-gulp.task('uglify', function() {
-  return gulp.src('_site/js/app.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('_site/js/'));
+gulp.task('js-watch', ['scripts'], function(done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('uglify', function () {
+    return gulp.src('_site/js/app.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('_site/js/'));
 });
 
 gulp.task('minify', function () {
-  return gulp.src('_site/css/main.css')
-    .pipe(sourcemaps.init())
-    .pipe(nano())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('_site/css/'));
+    return gulp.src('_site/css/main.css')
+        .pipe(nano())
+        .pipe(gulp.dest('_site/css/'));
 });
-
 
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-  return gulp.src('_sass/main.scss')
-    .pipe(sass()
-      .on('error', sass.logError))
-    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-    .pipe(gulp.dest('_site/css/'))
-    .pipe(browserSync.reload({stream:true}))
-    .pipe(gulp.dest('css'));
+    return gulp.src('_sass/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass()
+            .on('error', sass.logError))
+        .pipe(prefix(['last 15 versions', '> 1%'], {cascade: true}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('_site/css/'))
+        .pipe(browserSync.stream())
+        .pipe(gulp.dest('css'));
 });
+
 
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-  gulp.watch(['_sass/*.scss', 'css/*.scss'], ['sass']);
-  gulp.watch(['*.md','*.html', '_layouts/*.html', '_posts/*', '_includes/*', '_portfolio/*', 'pages/*'], ['jekyll-rebuild']).on('change', browserSync.reload);
-  gulp.watch(['assets/**/*.{jpg,png,gif}'], ['jekyll-rebuild']);
-  gulp.watch(['_js/**/*.js'], ['scripts']).on('change', browserSync.reload);
+    gulp.watch(['_sass/*.scss', 'css/*.scss'], ['sass']);
+    gulp.watch(['*.md', '*.html', '_layouts/*.html', '_posts/*', '_includes/*', '_portfolio/*', 'pages/*'], ['jekyll-rebuild']);//.on('change', browserSync.reload);
+    gulp.watch(['assets/**/*.{jpg,png,gif}'], ['jekyll-rebuild']);
+    gulp.watch(['_js/**/*.js'], ['js-watch']);
 
 });
 
@@ -114,11 +120,10 @@ gulp.task('watch', function () {
  * Watch for new images and minifiy them
  */
 gulp.task('imagemin', function () {
-  return gulp.src('_site/assets/**/*.{jpg,png,gif}')
-    .pipe(imagemin())
-    .pipe(gulp.dest('_site/assets/'));
+    return gulp.src('_site/assets/**/*.{jpg,png,gif}')
+        .pipe(imagemin())
+        .pipe(gulp.dest('_site/assets/'));
 });
-
 
 
 /**
